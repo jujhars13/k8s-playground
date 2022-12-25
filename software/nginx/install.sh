451b151ed5c3cd02fa5366a4d902cf06d584d35d
Jujhar Singh <jujhar@jujhar.com>
2022-12-25 13:26:18 +0000

    sets up a local app for testing

--- software/nginx/install.sh
+++ software/nginx/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 helm upgrade --install ingress-nginx ingress-nginx \
-  --repo https://kubernetes.github.io/ingress-nginx \
-  --namespace ingress-nginx --create-namespace
+    --repo https://kubernetes.github.io/ingress-nginx \
+    --namespace ingress-nginx \
+    --create-namespace \
+    --set controller.metrics.enabled=true \
+    --set-string controller.podAnnotations."prometheus\.io/scrape"="true" \
+    --set-string controller.podAnnotations."prometheus\.io/port"="10254"
--- software/prometheus/install.sh
+++ software/prometheus/install.sh
@@ -17,3 +17,6 @@ kubectl --namespace prometheus \
 # Get the PushGateway URL by running these commands in the same shell:
 export POD_NAME=$(kubectl get pods --namespace prometheus -l "app=prometheus-pushgateway,component=pushgateway" -o jsonpath="{.items[0].metadata.name}")
 kubectl --namespace prometheus port-forward $POD_NAME 9091
+
+# The Prometheus server can be accessed via port 80 on the following DNS name from within your cluster:
+# prometheus-server.prometheus.svc.cluster.local
